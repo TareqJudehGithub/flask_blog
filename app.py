f3335ade09dcda28ce83881a1f7fba73ef61626f
Tareq Judeh <tareq.judeh@outlook.com>
2021-02-23 21:02:31 +0200

    Intial commit.

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+
+# Create a flask instance
+app = Flask(__name__)
+
+
+# Create a route decorator
+@app.route('/')
+def index():
+    """Main webpage"""
+    first_name = "john smith"
+    stuff = "This is <strong>bold text</strong>."
+
+    return render_template(
+        "index.html",
+        first_name=first_name,
+        stuff=stuff
+    )
+
+
+# Create a custom route
+@app.route("/user/<string:name>")
+def user(name):
+    return render_template("user.html", user_name=name)
+
+
+# Creating a grocery list route page:
+@app.route("/grocery")
+def grocery():
+    grocery_list = ["bread", "milk", "cereal", "peanut butter", 41]
+    return render_template("grocery.html", grocery=grocery_list)
+
+
+# Create custom error pages
+# 1. Invalid URL
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("500.html"), 500
+
+
+if __name__ == '__main__':
+    app.run()
